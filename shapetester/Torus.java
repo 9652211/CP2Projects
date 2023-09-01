711da7b15e10c6026280878fe33314121b8dd021
9652211 <89046778+9652211@users.noreply.github.com>
2023-08-31 22:37:07 -0600

    Add files via upload

--- shapetester/Torus.java
+++ shapetester/Torus.java
@@ -1,8 +1,15 @@
 import java.lang.Math;
 public class Torus {
-    private double majorRadius = 0.0;
-    private double minorRadius = 0.0;
-    public static double getVolume(double R, double r) {
+    private double R = 0.0;
+    private double r = 0.0;
+    public void setDims(double majorRadius, double minorRadius) {
+        this.R = majorRadius;
+        this.r = minorRadius;
+    }
+    public double getVolume() {
         return 2*Math.pow(Math.PI*r, 2)*R;
+    }
+    public double getSurfaceArea() {
+        return 4*Math.pow(Math.PI, 2)*R*r;
     }
 }
