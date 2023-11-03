f962cfeeaa9d536a153aaed2ca8d1166c24105a4
DalinarKholin <89046778+9652211@users.noreply.github.com>
2023-11-02 21:54:18 -0600

    Add files via upload

---
+++ Inheritance/Races/Humanoid/Elf/Vanyar/Vanyar.java
@@ -0,0 +1,18 @@
+
+public class Vanyar extends Elf {
+
+	public Vanyar() {
+		String occupation;
+		
+		public void sing() {
+			println("A Elbereth Gilthoniel");
+			println("silivren penna miriel");
+			println("o menel aglar elenath!");
+			println("Na-chaered palan-diriel");
+			println("o galadhremmin ennorath,");
+			println("Fanuilos, le linnathon");
+			println("nef aear, si nef aearon!");
+		}
+	}
+
+}
