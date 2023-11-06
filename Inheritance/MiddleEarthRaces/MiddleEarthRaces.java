926de930b4d219f3c984841cc3c6a141d3fa4a3a
DalinarKholin <89046778+9652211@users.noreply.github.com>
2023-11-06 08:50:09 -0700

    Add files via upload

--- Inheritance/MiddleEarthRaces/MiddleEarthRaces.java
+++ Inheritance/MiddleEarthRaces/MiddleEarthRaces.java
@@ -3,7 +3,27 @@ public class MiddleEarthRaces {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Vanyar elenwe = new Vanyar();
+		elenwe.name = "Elenwë";
+		elenwe.spouse = "Turgon";
+		elenwe.children.add("Elwing");
+		elenwe.sing();
+		Noldor galadriel = new Noldor();
+		galadriel.name = "Galadriel";
+		galadriel.maternal = "Earwen";
+		galadriel.paternal = "Finarfin";
+		galadriel.children.add("Celebrian");
+		galadriel.spouse = "Celeborn";
+		galadriel.house = "Finwe";
+		Teleri earwen = new Teleri();
+		earwen.name = "Earwen";
+		earwen.spouse = "Finarfin";
+		earwen.paternal = "Olwe";
+		earwen.children.add("Finrod");
+		earwen.children.add("Angrod");
+		earwen.children.add("Aegnor");
+		earwen.children.add("Galadriel");
+		earwen.location = "Alqualonde";
 	}
 
 }
