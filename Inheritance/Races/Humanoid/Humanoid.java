c5c4df784fa7594bb4601375dd73843183f803c7
DalinarKholin <89046778+9652211@users.noreply.github.com>
2023-11-06 08:51:20 -0700

    Add files via upload

--- Inheritance/Races/Humanoid/Humanoid.java
+++ Inheritance/Races/Humanoid/Humanoid.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
 
 public class Humanoid extends Races {
-
+	protected String maternal;
+	protected String paternal;
+	protected String spouse;
+	ArrayList<String> children = new ArrayList<String>();
+	
 	public Humanoid() {
-		String[] parents;
-		String[] children;
+		maternal = "";
+		paternal = "";
+		spouse = "";
 	}
 
 }
