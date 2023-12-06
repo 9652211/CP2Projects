406a1f50e1d25e85fd81c75a07bc510d1a3b9dff
DalinarKholin <89046778+9652211@users.noreply.github.com>
2023-12-06 08:18:33 -0700

    Add files via upload

---
+++ Shapes/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+	Triangle tri = new Triangle();
+	tri.setA(1);
+	tri.setB(1);
+	tri.setC(1);
+	System.out.println(tri.area());
+}
---
+++ Shapes/Triangle.java
@@ -0,0 +1,21 @@
+
+public class Triangle implements Shape {
+	
+	private double a;
+	private double b;
+	private double c;
+	public void setA(double a) {
+		this.a = a;
+	}
+	public void setB(double b) {
+		this.b = b;
+	}
+	public void setC(double c) {
+		this.c = c;
+	}
+	@Override
+	public double area() {
+		double s = (a+b+c)/2;
+		return Math.pow(s*(s-a)*(s-b)*(s-c), 1/2);
+	}
+}
