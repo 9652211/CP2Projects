376689ef959ecf776f3a609fb517b014a479acd0
DalinarKholin <89046778+9652211@users.noreply.github.com>
2024-02-05 08:49:45 -0700

    Add files via upload

--- Caesar/Main.java
+++ Caesar/Main.java
@@ -1,15 +1,26 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		String input = "Welcome to the Caesar Cipher!";
-		System.out.println(input);
-		System.out.println(encrypt(input, 5));
+		try {
+			File input = new File("/Users/9652211/eclipse-workspace/Caesar/src/INPUT.txt");
+			FileReader reader = new FileReader(input);
+			int c;
+			String inputString = "";
+			while ((c = reader.read()) !=-1) {
+				char ch = (char) c;
+				inputString = inputString+ch;
+			}
+			System.out.println(inputString);
+			System.out.println(encrypt(inputString, 5));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static char[] alphabet = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
